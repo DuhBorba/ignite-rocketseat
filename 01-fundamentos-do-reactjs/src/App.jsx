009778fe2593850300603fafe0186df8daa8a6a7
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-04-13 16:19:07 -0300

    Create Properties Post

--- 01-fundamentos-do-reactjs/src/App.jsx
+++ 01-fundamentos-do-reactjs/src/App.jsx
@@ -6,6 +6,36 @@ import styles from './App.module.css';
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/DuhBorba.png',
+      name: 'Eduardo Borba',
+      role: 'Front-end Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Eaii pessoal 👋'},
+      {type: 'paragraph', content: 'Aqui é o Borba, queria compartilhar com vocês que finalizei um projeto em ReactJS!'},
+      {type: 'link', content: '#ReactJS'},
+    ],
+    publishedAt: new Date('2023-04-13 15:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @RocketSeat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-04-10 10:30:00')
+  },
+]
 
 function App() {
 
@@ -15,8 +45,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Eduardo Borba" content="Teste post descrição" />
-          <Post author="Eduardo Borba" content="Teste post descrição" />
+          {posts.map(post => {
+            return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+            )
+          })}
         </main>
       </div>
     </div>
