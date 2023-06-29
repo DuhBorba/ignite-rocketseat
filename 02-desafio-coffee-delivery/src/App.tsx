c301dcdefe7e36d82141adf2f6a08daabe0f29f0
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-06-29 16:39:22 -0300

    Config Global Style

--- 02-desafio-coffee-delivery/src/App.tsx
+++ 02-desafio-coffee-delivery/src/App.tsx
@@ -1,10 +1,14 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <p>App</p>
+      <div>
+        <p>App</p>
+      </div>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
