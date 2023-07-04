893b283b9ed72c3916d494c63f185430fb062588
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-07-04 16:13:40 -0300

    Create Routes

--- 02-desafio-coffee-delivery/src/App.tsx
+++ 02-desafio-coffee-delivery/src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        <p>App</p>
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
