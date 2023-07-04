93971998530a58e921630e0516da272cb70dc3bd
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-07-04 17:04:59 -0300

    Header Default Layout

--- 02-desafio-coffee-delivery/src/Router.tsx
+++ 02-desafio-coffee-delivery/src/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/Home'
 import { Success } from './pages/Success'
 import { Checkout } from './pages/Checkout'
@@ -6,9 +7,11 @@ import { Checkout } from './pages/Checkout'
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/success" element={<Success />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
+      </Route>
     </Routes>
   )
 }
